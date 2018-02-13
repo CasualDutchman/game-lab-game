a270e564444c766e9829f61996c6519d0f553a5e
Pieter <pieterj@live.nl>
2018-02-13 12:15:45 +0100

    localization

---
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LocalizationManager : MonoBehaviour {
+
+    public class LocalizationData {
+        public string key;
+        public string value;
+    }
+
+    public static LocalizationManager instance;
+
+    string missingKeyString = "Missing Localized Info";
+    public string languageIdName = "EN_us";
+
+    Dictionary<string, string> localizedText;
+
+    bool isReady = false;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }else {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+	void Start () {
+        OnLoadLocalizationFile(languageIdName);
+    }
+	
+	void OnLoadLocalizationFile(string languageCode) {
+        localizedText = new Dictionary<string, string>();
+        string filePath = Path.Combine(Application.streamingAssetsPath, languageCode + ".txt");
+
+        if (File.Exists(filePath)) {
+            string[] dataArray = File.ReadAllLines(filePath);
+            foreach (string data in dataArray) {
+                string[] keyValue = data.Split('=');
+                localizedText.Add(keyValue[0], keyValue[1]);
+            }
+
+            Debug.Log("Localized content loaded. Dictionary contains " + localizedText.Count + " entries");
+        }
+        else {
+            Debug.LogError(languageCode + ".txt does not excist in the StreamingAssets folder.");
+        }
+
+        isReady = true;
+    }
+
+    public string GetLocalizedValue(string key) {
+        string result = missingKeyString + ": key = " + key;
+        if (localizedText.ContainsKey(key)) {
+            result = localizedText[key];
+        }
+
+        return result;
+    }
+
+    public bool IsReady() {
+        return isReady;
+    }
+}
---
+++ Assets/Scripts/Menu/Options/OptionsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsManager : MonoBehaviour {
+
+    public static OptionsManager instance;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
